05c7d381a8ce2493749bbf0b2953f00e4e04e8d7
mikayilov2006@gmail.com <Fuadmikayil>
2024-09-14 11:46:06 +0400

    querySelectorAll

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 //const h1_tag = document.getElementsByTagName("h1");
 const h1_tag = document.getElementById("demo");
 console.log(h1_tag);
+const h2_tag = document.querySelectorAll("demo");
+console.log(h2_tag);
