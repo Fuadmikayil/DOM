ace5ed8fef016f071f72200ef3d60b39aa4907ba
mikayilov2006@gmail.com <Fuadmikayil>
2024-09-15 11:10:37 +0400

    remove and add metod

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const h2_tag = document.querySelectorAll("#demo");
 console.log(h2_tag.textContent);
 console.log(h2_tag.innerText);
 console.log(h2_tag.innerHtml);
+///////////////////////////
+const div_tag = document.querySelector(".wrapper")
+console.log(div_tag);
+const p_tag = document.querySelector("#memo")
+p_tag.classList.add("hakuna");
+p_tag.classList.remove("hakuna");
 
