bb051ee10b4175c553700975f19433b93ea3b723
mikayilov2006@gmail.com <Fuadmikayil>
2024-09-15 13:02:46 +0400

    demo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//const h1_tag = document.getElementsByTagName("h1");
+/* //const h1_tag = document.getElementsByTagName("h1");
 const h1_tag = document.getElementById("demo");
 console.log(h1_tag);
 const h2_tag = document.querySelectorAll("#demo");
@@ -9,11 +9,26 @@ console.log(h2_tag.innerHtml);
 const div_tag = document.querySelector(".wrapper")
 console.log(div_tag);
 const p_tag = document.querySelector("#memo")
-p_tag.classList.add("hakuna");
+
 p_tag.classList.remove("hakuna");
 p_tag.classList.toggle("hakuna");
 const h4_tag = document.createElement("h4");
 h4_tag.innerText= "hakuna matat"
 console.log(h4_tag);
-console.log(h4_tag);
+const div_tag1 = document.querySelector(".wrapper")
+div_tag.appendChild(div_tag1) */
+////////////////////////
+const btnAdd = document.querySelector("#btnAdd")
+const btnRemove = document.querySelector("#btnRemove")
+const btnToggle = document.querySelector("#btnToggle")
+const h1_tag = document.querySelector("#txt")
 
+btnAdd.addEventListener("click",() =>{
+    h1_tag.classList.add("hakuna")
+})
+btnRemove.addEventListener("click",() =>{
+    h1_tag.classList.remove("hakuna")
+})
+btnToggle.addEventListener("click",() =>{
+    h1_tag.classList.toggle("hakuna")
+})
