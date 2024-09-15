d4c86b2f4dfe114bb8216939fae0307250328189
mikayilov2006@gmail.com <Fuadmikayil>
2024-09-15 10:56:10 +0400

    textContent and innerText andinnerHtml

--- app.js
+++ app.js
@@ -2,4 +2,7 @@
 const h1_tag = document.getElementById("demo");
 console.log(h1_tag);
 const h2_tag = document.querySelectorAll("#demo");
-console.log(h2_tag);
+console.log(h2_tag.textContent);
+console.log(h2_tag.innerText);
+console.log(h2_tag.innerHtml);
+
